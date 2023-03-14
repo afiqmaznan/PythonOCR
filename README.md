68b325b9f9b6a7481116be7b75a959ef8c2e2a82
Afiq <afiq.capsphere@gmail.com>
2023-03-14 16:41:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PythonOCR
+Python OCR is used to scan PDF files into a text. Using *pdf2image* to convert PDF to image and *pytesseract* to scan all the texts in images.
---
+++ main.py
@@ -0,0 +1,19 @@
+import pytesseract
+from pdf2image import convert_from_path
+import glob
+
+
+def lambda_handler():
+    # PDF file in your local
+    pdfs = glob.glob(r"C:/Users/Documents/test.pdf")
+
+    # Tesseract path in your local
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+    for pdf_path in pdfs:
+        # Poppler path in your local
+        pages = convert_from_path(pdf_path, 500, poppler_path = r"C:\Program Files\poppler-0.68.0\bin", )
+
+        for pageNum, imgBlob in enumerate(pages):
+            text = pytesseract.image_to_string(imgBlob, lang='eng')
+            print(text)
